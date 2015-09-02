921ab69dec2db383b37a6d0e809183c89ff5bcf1
jurin-n <jurin.nakano@gmail.com>
2015-09-02 23:23:08 +0900

    add java.io.InputStreamReader Sample code.

---
+++ src/main/java/com/jurin_n/learning_java_se/net/InputStreamReaderTest.java
@@ -0,0 +1,20 @@
+package com.jurin_n.learning_java_se.net;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class InputStreamReaderTest {
+	public static void main(String[] args) throws IOException{
+		URL url = new URL("http://dokojava.jp");
+		InputStream is = url.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		int i = isr.read();
+		while(i != -1){
+			System.out.print((char)i);
+			i = isr.read();
+		}
+		isr.close();
+	}
+}
