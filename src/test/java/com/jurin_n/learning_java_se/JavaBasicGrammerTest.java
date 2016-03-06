4ffd764c48997a10a71c492688c1d432fa89f0ed
jurin-n <jurin.nakano@gmail.com>
2016-03-06 16:40:35 +0900

    add

---
+++ src/test/java/com/jurin_n/learning_java_se/JavaBasicGrammerTest.java
@@ -0,0 +1,24 @@
+package com.jurin_n.learning_java_se;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JavaBasicGrammerTest {
+
+	@Test
+	public void test() {
+		fail();
+		int a = 100;
+		long b = 312345L; //3.12345L はだめ。
+		int c = 0777;
+		int d = 0xDeed;
+		int e = 0_52;
+		System.out.println("a=" + a);
+		System.out.println("b=" + b);
+		System.out.println("c=" + c);
+		System.out.println("d=" + d);
+		System.out.println("e=" + e);
+	}
+
+}
